32c9e5e68b2f875ebab8601e295e2ca485ef1c2c
Leo <leo881003@gmail.com>
2021-03-06 22:20:55 +0800

    Add ipc module

---
+++ wirectl/src/ipc/mod.rs
@@ -0,0 +1,6 @@
+//! Inter-process API (For userspace)
+//!
+//! This is the API to communicate with userspace wireguard implementation.
+//! It communicates with programs such as `wireguard-go` by unix domain socket.
+//!
+//! For more detail protocol, read the [documentation](https://www.wireguard.com/xplatform/) by wireguard.
