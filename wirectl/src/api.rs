05b60c45fbfcf1e191977e8e71c99901bf61991d
Leo <leo881003@gmail.com>
2021-05-15 23:54:19 +0800

    Impl public interface get/list api

--- wirectl/src/api.rs
+++ wirectl/src/api.rs
@@ -1,6 +1,16 @@
 use crate::{ipc, types::WgDevice};
 use crate::WireCtlError;
 
+cfg_if! {
+    if #[cfg(target_os = "linux")] {
+        pub const AVAILABLE_WG_APIS: [WgApi; 1] = [WgApi::IPC];
+    } else if #[cfg(any(target_os = "openbsd", target_os = "freebsd"))] {
+        pub const AVAILABLE_WG_APIS: [WgApi; 1] = [WgApi::IPC];
+    } else {
+        pub const AVAILABLE_WG_APIS: [WgApi; 1] = [WgApi::IPC];
+    }
+}
+
 /// Used by [`crate::interface::WgInterface`] to determine which underlaying API to use
 #[allow(clippy::upper_case_acronyms)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -13,7 +23,7 @@ pub enum WgApi {
 }
 
 impl WgApi {
-    pub(crate) async fn list_devices(&self) -> Result<Vec<String>, WireCtlError> {
+    pub(crate) async fn list_devices(self) -> Result<Vec<String>, WireCtlError> {
         match self {
             WgApi::IPC => ipc::list_devices().await,
             #[cfg(target_os = "linux")]
@@ -23,7 +33,7 @@ impl WgApi {
         }
     }
 
-    pub(crate) async fn get_device(&self, ifname: &str) -> Result<WgDevice, WireCtlError> {
+    pub(crate) async fn get_device(self, ifname: &str) -> Result<WgDevice, WireCtlError> {
         match self {
             WgApi::IPC => ipc::get_device(ifname).await,
             #[cfg(target_os = "linux")]
@@ -33,7 +43,7 @@ impl WgApi {
         }
     }
 
-    pub(crate) async fn set_device(&self) -> Result<Vec<String>, WireCtlError> {
+    pub(crate) async fn set_device(self) -> Result<Vec<String>, WireCtlError> {
         match self {
             WgApi::IPC => todo!(),
             #[cfg(target_os = "linux")]
--- wirectl/src/interface.rs
+++ wirectl/src/interface.rs
@@ -1,8 +1,33 @@
 //! Types related to Wireguard devices
-use crate::api::WgApi;
+use futures::{StreamExt, TryFutureExt, TryStreamExt};
+
+use crate::{
+    api::{WgApi, AVAILABLE_WG_APIS},
+    WireCtlError,
+};
 
 #[derive(Debug)]
 pub struct WgInterface {
     ifname: String,
     wgapi: WgApi,
+}
+
+impl WgInterface {
+    pub async fn get_interfaces() -> Result<Vec<WgInterface>, WireCtlError> {
+        futures::stream::iter(AVAILABLE_WG_APIS.iter().copied())
+            .then(|api| api.list_devices().map_ok(move |l| {
+                l.into_iter()
+                    .map(|ifname| WgInterface { ifname, wgapi: api })
+                    .collect()
+            }))
+            .try_concat()
+            .await
+    }
+
+    pub async fn list_interfaces() -> Result<Vec<String>, WireCtlError> {
+        futures::stream::iter(&AVAILABLE_WG_APIS)
+            .then(|api| api.list_devices())
+            .try_concat()
+            .await
+    }
 }
